8a7e1c40ee7ad8ebaffd27f1e113c254817030b3
kaidahousecat <kai85@wtnet.de>
2016-09-18 21:52:55 +0200

    try block added

--- ScrollViewCocosSharp/ScrollViewCocosSharp/SpriteSheet/SpriteSheetCreator.cs
+++ ScrollViewCocosSharp/ScrollViewCocosSharp/SpriteSheet/SpriteSheetCreator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using CocosSharp;
 
@@ -27,90 +28,98 @@ namespace ScrollViewCocosSharp.SpriteSheet
         /// <returns></returns>
         public bool RenderGraphics()
         {
-            var actualPosition = new CCPoint(0, 0);
-            int highestY = 0;
-
-            foreach (var keyAndDrawNode in _drawNodes)
+            try
             {
-                var drawNode = keyAndDrawNode.Value;
+                var actualPosition = new CCPoint(0, 0);
+                int highestY = 0;
 
-                //new line
-                if ((actualPosition.X + drawNode.BoundingBox.Size.Width) > RenderTextureMaxWidthAndHeight)
+                foreach (var keyAndDrawNode in _drawNodes)
                 {
-                    actualPosition.X = 0;
-                    actualPosition.Y = highestY;
+                    var drawNode = keyAndDrawNode.Value;
+
+                    //new line
+                    if ((actualPosition.X + drawNode.BoundingBox.Size.Width) > RenderTextureMaxWidthAndHeight)
+                    {
+                        actualPosition.X = 0;
+                        actualPosition.Y = highestY;
+                    }
+
+                    //remember highest y, to avoid intersection
+                    if ((actualPosition.Y + drawNode.BoundingBox.Size.Height) > highestY)
+                    {
+                        highestY = (int)(actualPosition.Y + drawNode.BoundingBox.Size.Height + 1);
+                    }
+
+                    var rectPosition = new CCRect(actualPosition.X, actualPosition.Y, drawNode.BoundingBox.Size.Width, drawNode.BoundingBox.Size.Height);
+
+                    drawNode.Position = new CCPoint(rectPosition.MinX, rectPosition.MinY);
+
+                    _rectsForSprites.Add(keyAndDrawNode.Key, rectPosition);
+
+                    actualPosition.X += (drawNode.BoundingBox.Size.Width + 1);
                 }
 
-                //remember highest y, to avoid intersection
-                if ((actualPosition.Y + drawNode.BoundingBox.Size.Height) > highestY)
+                foreach (var keyAndLabel in _labels)
                 {
-                    highestY = (int)(actualPosition.Y + drawNode.BoundingBox.Size.Height + 1);
+                    var label = keyAndLabel.Value;
+
+                    //new line
+                    if ((actualPosition.X + label.BoundingBox.Size.Width) > RenderTextureMaxWidthAndHeight)
+                    {
+                        actualPosition.X = 0;
+                        actualPosition.Y = highestY;
+                    }
+
+                    //remember highest y, to avoid intersection
+                    if ((actualPosition.Y + label.BoundingBox.Size.Height) > highestY)
+                    {
+                        highestY = (int)(actualPosition.Y + label.BoundingBox.Size.Height + 1);
+                    }
+
+                    var rectPosition = new CCRect(actualPosition.X, actualPosition.Y, label.BoundingBox.Size.Width, label.BoundingBox.Size.Height);
+
+                    label.Position = rectPosition.Center;
+
+                    _rectsForSprites.Add(keyAndLabel.Key, rectPosition);
+
+                    actualPosition.X += (label.BoundingBox.Size.Width + 1);
                 }
 
-                var rectPosition = new CCRect(actualPosition.X, actualPosition.Y, drawNode.BoundingBox.Size.Width, drawNode.BoundingBox.Size.Height);
+                //render the drawn graphic in one big texture
+                _renderTexture = new CCRenderTexture(new CCSize(RenderTextureMaxWidthAndHeight, highestY), new CCSize(RenderTextureMaxWidthAndHeight, highestY));
+                _renderTexture.Begin();
 
-                drawNode.Position = new CCPoint(rectPosition.MinX, rectPosition.MinY);
-
-                _rectsForSprites.Add(keyAndDrawNode.Key, rectPosition);
-
-                actualPosition.X += (drawNode.BoundingBox.Size.Width + 1);
-            }
-
-            foreach (var keyAndLabel in _labels)
-            {
-                var label = keyAndLabel.Value;
-
-                //new line
-                if ((actualPosition.X + label.BoundingBox.Size.Width) > RenderTextureMaxWidthAndHeight)
+                foreach (var keyAndDrawNode in _drawNodes)
                 {
-                    actualPosition.X = 0;
-                    actualPosition.Y = highestY;
+                    keyAndDrawNode.Value.Visit();
                 }
 
-                //remember highest y, to avoid intersection
-                if ((actualPosition.Y + label.BoundingBox.Size.Height) > highestY)
+                foreach (var keyAndLabel in _labels)
                 {
-                    highestY = (int)(actualPosition.Y + label.BoundingBox.Size.Height + 1);
+                    keyAndLabel.Value.Visit();
                 }
 
-                var rectPosition = new CCRect(actualPosition.X, actualPosition.Y, label.BoundingBox.Size.Width, label.BoundingBox.Size.Height);
+                _renderTexture.End();
 
-                label.Position = rectPosition.Center;
 
-                _rectsForSprites.Add(keyAndLabel.Key, rectPosition);
+                //mirror all cordinates vertical...nessecary as the texture use another coordinate system
+                Dictionary<object, CCRect> verticalMirrorCoordsDrawNode = new Dictionary<object, CCRect>();
+                foreach (var keyAndDrawRect in _rectsForSprites)
+                {
+                    CCRect spriteRect = new CCRect(keyAndDrawRect.Value.MinX, highestY - keyAndDrawRect.Value.MaxY, keyAndDrawRect.Value.Size.Width, keyAndDrawRect.Value.Size.Height);
+                    verticalMirrorCoordsDrawNode.Add(keyAndDrawRect.Key, spriteRect);
+                }
 
-                actualPosition.X += (label.BoundingBox.Size.Width + 1);
+                _rectsForSprites = verticalMirrorCoordsDrawNode;
+
             }
-
-            //render the drawn graphic in one big texture
-            _renderTexture = new CCRenderTexture(new CCSize(RenderTextureMaxWidthAndHeight, highestY), new CCSize(RenderTextureMaxWidthAndHeight, highestY));
-            _renderTexture.Begin();
-
-            foreach (var keyAndDrawNode in _drawNodes)
+            catch (Exception e)
             {
-                keyAndDrawNode.Value.Visit();
+                Debug.WriteLine(e);
+                return false;
             }
-
-            foreach (var keyAndLabel in _labels)
-            {
-                keyAndLabel.Value.Visit();
-            }
-
-            _renderTexture.End();
-
-
-            //mirror all cordinates vertical...nessecary as the texture use another coordinate system
-            Dictionary<object, CCRect> verticalMirrorCoordsDrawNode = new Dictionary<object, CCRect>();
-            foreach (var keyAndDrawRect in _rectsForSprites)
-            {
-                CCRect spriteRect = new CCRect(keyAndDrawRect.Value.MinX, highestY - keyAndDrawRect.Value.MaxY, keyAndDrawRect.Value.Size.Width, keyAndDrawRect.Value.Size.Height);
-                verticalMirrorCoordsDrawNode.Add(keyAndDrawRect.Key, spriteRect);
-            }
-
-            _rectsForSprites = verticalMirrorCoordsDrawNode;
 
             _successfullyRendered = true;
-
             return true;
         }
 
